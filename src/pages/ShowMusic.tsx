239442f2ff95796310acb0a2b2a697a3ad82a80b
Matheus dos Santos Bonifcio <caneladeouro10@gmail.com>
2020-12-10 00:02:09 -0300

    Score_10122020_00H02

--- src/pages/ShowMusic.tsx
+++ src/pages/ShowMusic.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
+import {
+  TextField,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@material-ui/core";
+
 // import CircularProgress from "@material-ui/core/CircularProgress";
 // import LinearProgress from "@material-ui/core/LinearProgress";
 
@@ -13,56 +16,99 @@ import UseStyles from "../styles/appShowMusic";
 
 const useStyles = UseStyles();
 
+interface DataMusicLetter {
+  art: {
+    name: string;
+    pic_small: string;
+    pic_medium: string;
+  };
+  mus: {
+    text: string;
+    translate: {
+      text: string;
+    };
+  };
+}
+
+function getMusicLetter(url: string) {
+  var dataMusicLetter: DataMusicLetter = {
+    art: { name: "", pic_medium: "", pic_small: "" },
+    mus: {
+      text: "",
+      translate: {
+        text: "",
+      },
+    },
+  };
+
+  fetch(url)
+    .then((res: any) => res.json())
+    .then((data: any) => {
+      if (data != dataMusicLetter) {
+        throw "Error";
+      }
+      dataMusicLetter = data;
+    })
+    .catch((err: any) => {});
+  return dataMusicLetter;
+}
+
 export default function ShowMusicComponent(): JSX.Element {
-  const classes = useStyles();
+  const classes = useStyles.MakeStyles();
   const bull = <span className={classes.bullet}>•</span>;
+  const classesTwo = useStyles.CreateStyles();
+  const dataMusicLetter = getMusicLetter(
+    "http://localhost/PWII/Projects/projeto-001-integrando-com-api/backend/src/getMusicLetter.php"
+  ).mus.translate.text;
 
   return (
     <div className={classes.content}>
-      <div className={classes.divImage}>
-        <img src="" alt="imagem do autor" className={classes.image} />
+      <div className={classes.image}>
+        <img src="" alt="Imagem do autor" />
       </div>
-      <form noValidate autoComplete="off" className={classes.formContent}>
-        <TextField id="standard-basic" label="Standard" />
-        <TextField id="filled-basic" label="Filled" variant="filled" />
+
+      <form className={classesTwo.form} noValidate autoComplete="off">
         <TextField
           id="outlined-basic"
-          className={classes.formContentField}
-          label="Outlined"
+          label="Autor"
           variant="outlined"
+          className={classes.input}
         />
-      </form>
-
-      <div className={classes.divCardMusic}>
-        <Card className={classes.cardMusic}>
-          <CardContent>
-            <Typography
-              className={classes.title}
-              color="textSecondary"
-              gutterBottom
-            >
-              Word of the Day
-            </Typography>
-            <Typography variant="h5" component="h2">
-              be{bull}nev{bull}o{bull}lent
-            </Typography>
-            <Typography className={classes.pos} color="textSecondary">
-              adjective
-            </Typography>
-            <Typography variant="body2" component="p">
-              well meaning and kindly.
-              <br />
-              {'"a benevolent smile"'}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Algo</Button>
-          </CardActions>
-        </Card>
-        <Button variant="contained" className={classes.cardButton}>
-          Algo
+        <TextField
+          id="outlined-basic"
+          label="Musica"
+          variant="outlined"
+          className={classes.input}
+        />
+        <Button variant="contained" color="primary" className={classes.input}>
+          Procurar
         </Button>
-      </div>
+      </form>
+      <Card className={classes.card} variant="outlined">
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            Word of the Day
+          </Typography>
+          <Typography variant="h5" component="h2">
+            be{bull}nev{bull}o{bull}lent
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            adjective
+          </Typography>
+          <Typography variant="body2" component="p">
+            {dataMusicLetter}
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
--- src/styles/appShowMusic.ts
+++ src/styles/appShowMusic.ts
@@ -1,68 +1,64 @@
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
 export default function AppShowMusic() {
-  return makeStyles({
-    content: {
-      width: "100vw",
-      height: "100vh",
-      backgroundColor: "#000000",
-    },
-    divImage: {
-      marginTop: 56,
-      marginLeft: 15,
-      width: "20vw",
-      height: "20vw",
-      backgroundColor: "black",
-      border: "1px solid white",
-      boxShadow: "14px 14px 14px white",
-      borderRadius: "14px",
-    },
-    image: {
-      width: "100%",
-      height: "100%",
-    },
-    divCardMusic: {
-      height: "100%",
-    },
-    formContent: {
-      width: "48%",
-      height: "10%",
-      marginTop: 60,
-    },
-    formContentField: {
-      // backgroundColor: "#fff",
-      color: "#fff",
-    },
-    cardMusic: {
-      position: "absolute",
-      minWidth: 275,
-      width: "48%",
-      height: "70%",
-      backgroundColor: "#000000",
-      boxShadow: "14px 14px 14px #ffffff",
-      border: "1px solid #ffffff",
-      color: "#ffffff",
-      right: "5%",
-      top: "13%",
-    },
-    bullet: {
-      display: "inline-block",
-      margin: "0 2px",
-      transform: "scale(0.8)",
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-    cardButton: {
-      position: "absolute",
-      top: "90%",
-      right: "5%",
-      width: "48%",
-      backgroundColor: "crimson",
-      color: "white",
-    },
-  });
+  return {
+    MakeStyles: makeStyles({
+      content: {
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "#000000",
+        padding: "8% 0 0 3px",
+      },
+      image: {
+        height: "300px",
+        width: "300px",
+        border: "1px solid #ffffff",
+        borderRadius: "14px",
+      },
+      input: {
+        width: "500px",
+      },
+      card: {
+        minWidth: 275,
+        width: "48%",
+        height: "75%",
+        position: "absolute",
+        top: "12%",
+        right: "20px",
+        backgroundColor: "#000000",
+        border: "1px solid #ffffff",
+        color: "#ffffff",
+      },
+      bullet: {
+        display: "inline-block",
+        margin: "0 2px",
+        transform: "scale(0.8)",
+      },
+      title: {
+        fontSize: 14,
+        color: "#ffffff",
+      },
+      pos: {
+        marginBottom: 12,
+        color: "#ffffff",
+      },
+    }),
+    CreateStyles: makeStyles((theme: Theme) =>
+      createStyles({
+        form: {
+          "& > *": {
+            margin: theme.spacing(1),
+            width: "55ch",
+          },
+          alignItems: "center",
+          width: "48%",
+          borderRadius: "5px 5px 5px 0",
+          backgroundColor: "#ffffff",
+          marginTop: "5px",
+          display: "flex",
+          flexDirection: "column",
+        },
+      })
+    ),
+  };
 }
